a938c7ca7f3311de1a0c291bc64303cd735c233e
jrodg85 <63821662+jrodg85@users.noreply.github.com>
2023-02-28 12:08:25 +0100

    Optimización objetivo, entrega de practica.

--- jrodg85-appCaesar.py
+++ jrodg85-appCaesar.py
@@ -13,25 +13,23 @@ opcionMenu=""
 
 def noHayFraseEstudio():
         print("""No hay frase de estudio.
-Procediendo a introducir frase de estudio
+Procediendo a introducir frase de estudio.
 Una vez introducida la frase volvera al menu principal.""")
         introduzcaUnaFrase()
         
 def volverAMenuPrincipal():
+    print("Presione una tecla para continuar...")
     wait_for("")
     borrarPantalla()
     seleccionMenu()
 
-## Metodo de borrar pantalla encontrado en https://unipython.com/como-borrar-pantalla-en-python/
-def borrarPantalla(): #Definimos la función estableciendo el nombre que queramos
+def borrarPantalla(): # Metodo de borrar pantalla encontrado en https://unipython.com/como-borrar-pantalla-en-python/ Definimos la función estableciendo el nombre que queramos
     if os.name == "posix":
         os.system ("clear")
     elif os.name == "ce" or os.name == "nt" or os.name == "dos":
         os.system ("cls")
 
-## wait for importado de la siguiente url https://es.stackoverflow.com/questions/277815/como-hago-un-presione-enter-para-continuar-en-python-3-linux
-## De momento se queda comentado ya que en spider no funciona correctamente pero en la terminal funciona
-def wait_for(mess, *keys):
+def wait_for(mess, *keys):## wait for importado de la siguiente url https://es.stackoverflow.com/questions/277815/como-hago-un-presione-enter-para-continuar-en-python-3-linux En spider no funciona correctamente pero en la terminal funciona
     file_descriptor = sys.stdin.fileno()
     old = termios.tcgetattr(file_descriptor)
     new = old[:]
@@ -52,14 +50,12 @@ def introduzcaUnaFrase():
     fraseEstudio=input("Introduzca clave: ")
     borrarPantalla()
     verFraseEstudio()
-    print("Presione una tecla para continuar...")
     volverAMenuPrincipal()
 
-def contarConsonantesDeLaFrase():
+def contarConsonantesDeLaFrase():## metodo para contar consonantes en la URL https://parzibyte.me/blog/2020/10/18/python-contar-consonantes-cadena/
     if fraseEstudio == "":
         noHayFraseEstudio()
     else:
-        ## metodo para contar consonantes en la URL https://parzibyte.me/blog/2020/10/18/python-contar-consonantes-cadena/
         def es_vocal(letra):
             return letra in "aeiou"
         cadena = fraseEstudio
@@ -69,28 +65,24 @@ def contarConsonantesDeLaFrase():
         for letra in cadena_minuscula:
             if letra.isalpha() and not es_vocal(letra):
                 cantidad_consonantes += 1
-        print(f""""En la frase '{cadena}' hay {cantidad_consonantes} consonantes
-Presione una tecla para continuar...""")
+        print(f"En la frase '{cadena}' hay {cantidad_consonantes} consonantes.")
         volverAMenuPrincipal()
 
-def contarVocalesDeLaFrase():
+def contarVocalesDeLaFrase():## método para contar consonantes de la URL https://parzibyte.me/blog/2020/10/12/contar-vocales-python/
     if fraseEstudio == "":
         noHayFraseEstudio()
     else:
-        ## método para contar consonantes de la URL https://parzibyte.me/blog/2020/10/12/contar-vocales-python/
         contador = 0
         for letra in fraseEstudio:
             if letra.lower() in "aeiou":
                 contador += 1
-        print("En la frase '", fraseEstudio, "' hay ", contador, """ vocales"
-Presione una tecla para continuar...""")
+        print("En la frase '", fraseEstudio, "' hay ", contador," vocales.")
         volverAMenuPrincipal()
 
-def mostrarParaCadaLetraCuantasVecesSeRepite():
+def mostrarParaCadaLetraCuantasVecesSeRepite():# fuente para la realizacion del metodo tomada de la URL https://es.stackoverflow.com/questions/444688/contar-caracteres-repetidos-en-una-cadena
     if fraseEstudio == "":
         noHayFraseEstudio()
     else:
-    # fuente para la realizacion del metodo tomada de la URL https://es.stackoverflow.com/questions/444688/contar-caracteres-repetidos-en-una-cadena
         letras_dic = dict()  #Guarda repetición de letras
         for letra in fraseEstudio.lower(): #Por cada letra
             if letra in letras_dic: #Si ya estaba en el dic() significa que se repite
@@ -98,88 +90,64 @@ def mostrarParaCadaLetraCuantasVecesSeRepite():
                     letras_dic[letra] += 1 #Continua el conteo
             else:
                 letras_dic[letra] = 1 #Si la letra no esta en el diccionario, la agrega
-        print("""
-El conjunto de letras es el siguiente.
-""",letras_dic, """)
-Presione una tecla para continuar...""")
+        print("""El conjunto de letras es el siguiente:
+""",letras_dic)
         volverAMenuPrincipal()
 
-def codificadcionClaveCaesarRotacion2():
+def codificadcionClaveCaesarRotacion2():# Fuente utilizada para este apartado, es la siguiente URL https://parzibyte.me/blog/2018/12/10/cifrado-cesar-python/
     global opcionMenu
     if fraseEstudio == "":
-        print("""No hay frase de estudio.
-Procediendo a introducir frase de estudio
-Una vez introducida la frase volvera al menu principal.""")
-        introduzcaUnaFrase()
+        noHayFraseEstudio()
     else:
-        # Fuente utilizada para este apartado, es la siguiente URL https://parzibyte.me/blog/2018/12/10/cifrado-cesar-python/
         mensaje=fraseEstudio
         rotaciones=2
         alfabeto = "abcdefghijklmnopqrstuvwxyz"
         alfabeto_mayusculas = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
         longitud_alfabeto = len(alfabeto)
         codificado = ""
-        if opcionMenu=="5":
-            for letra in mensaje:
-                if not letra.isalpha() or letra.lower() == 'ñ':
-                    codificado += letra
-                    continue
-                valor_letra = ord(letra)
-                # Suponemos que es minúscula, así que esto comienza en 97(a) y se usará el alfabeto en minúsculas
-                alfabeto_a_usar = alfabeto
-                limite = 97  # Pero si es mayúscula, comienza en 65(A) y se usa en mayúsculas
-                if letra.isupper():
-                    limite = 65
-                    alfabeto_a_usar = alfabeto_mayusculas
-    
+        for letra in mensaje:
+            if not letra.isalpha() or letra.lower() == 'ñ':
+                codificado += letra
+                continue
+            valor_letra = ord(letra)
+            # Suponemos que es minúscula, así que esto comienza en 97(a) y se usará el alfabeto en minúsculas
+            alfabeto_a_usar = alfabeto
+            limite = 97  # Pero si es mayúscula, comienza en 65(A) y se usa en mayúsculas
+            if letra.isupper():
+                limite = 65
+                alfabeto_a_usar = alfabeto_mayusculas
+            if opcionMenu=="5":
                 # Rotamos la letra
                 posicion = (valor_letra - limite + rotaciones) % longitud_alfabeto
-    
                 # Convertimos el entero resultante a letra y lo concatenamos
                 codificado += alfabeto_a_usar[posicion]
-            print("La frase '", fraseEstudio, "' tiene como codigo la siguiente frase: \n", codificado,"""
-    Presione una tecla para continuar...""")
-            volverAMenuPrincipal()
-        elif opcionMenu=="6":
-            for letra in mensaje:
-                if not letra.isalpha() or letra.lower() == 'ñ':
-                    codificado += letra
-                    continue
-                valor_letra = ord(letra)
-                # Suponemos que es minúscula, así que esto comienza en 97(a) y se usará el alfabeto en minúsculas
-                alfabeto_a_usar = alfabeto
-                limite = 97  # Pero si es mayúscula, comienza en 65(A) y se usa en mayúsculas
-                if letra.isupper():
-                    limite = 65
-                    alfabeto_a_usar = alfabeto_mayusculas
-    
+            elif opcionMenu=="6":
                 # Rotamos la letra
                 posicion = (valor_letra - limite - rotaciones) % longitud_alfabeto
-    
                 # Convertimos el entero resultante a letra y lo concatenamos
                 codificado += alfabeto_a_usar[posicion]
-            print("La frase '", fraseEstudio, "' tiene decodificado la siguiente frase: \n", codificado,"""
-    Presione una tecla para continuar...""")
-            volverAMenuPrincipal()
+        if opcionMenu=="5":
+            print("La frase '", fraseEstudio, "' tiene como codigo la siguiente frase: \n", codificado)
+        elif opcionMenu=="6":    
+            print("La frase '", fraseEstudio, "' tiene decodificado la siguiente frase: \n", codificado)
+        volverAMenuPrincipal()
     
 def salirDeAppCaesar():
-    print("borrando datos almacenados")
+    print("Borrando datos almacenados.")
     global opcionMenu
     del (opcionMenu)
     global fraseEstudio
     del (fraseEstudio)
-    print("Adios")
+    print("Adios.")
     exit()
 
 def verFraseEstudio():
-    print ("La frase de estudio es: '", fraseEstudio,"""'
-Presione una tecla para continuar...""")
+    print ("La frase de estudio es: '", fraseEstudio, "'.")
     volverAMenuPrincipal()
 
 def seleccionMenu():
     global opcionMenu
-    print("""
-Seleccione una opción:
+    opcionMenu=input("""Seleccione una opción:
     1 ==> Introduzca una frase.
     2 ==> Contar cuantas consonantes hay en el texto.
     3 ==> Contar cuantas vocales hay en el texto.
@@ -188,8 +156,7 @@ Seleccione una opción:
     6 ==> Al insertar una frase codificada, esta la decodificará con clave 2.
     7 ==> Ver la frase de estudio.
     8 ==> Salir del programa.
-    """)
-    opcionMenu=input("Selecione Opcion: ")
+Opcion: """)
     if opcionMenu=="1":
         introduzcaUnaFrase()
     elif opcionMenu=="2":
@@ -205,7 +172,8 @@ Seleccione una opción:
     elif opcionMenu=="8":
         salirDeAppCaesar()
     else:
-        print("Seleccion incorrecta, ha seleccionado ", opcionMenu)
+        borrarPantalla()
+        print("Seleccion incorrecta, ha seleccionado '", opcionMenu,"'.")
         seleccionMenu()
 
 borrarPantalla()
